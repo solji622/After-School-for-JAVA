8d1014e0ffac72c62c7103ba7492675e1e5b01ff
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-09-21 07:44:22 +0900

    Create EX02.java

---
+++ Example/EX02.java
@@ -0,0 +1,13 @@
+package classExample;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        boolean newBoolean = (7>5) && (9<2) && (10==10);
+        System.out.println(newBoolean);
+//        식의 결정값이 이미 결정된 경우 자바는 연산을 멈춘다
+//        이와 같이 미리 멈추는 것을 '숏서킷현상'이라 한다
+        
+//        boolean new = m1() || m2() || m3();
+        
+    }
+}
