756e77db7b7170b0203ee2423b4fb631b7d9d7b6
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-10-24 18:18:13 +0900

    Create _06_ClassMethod.java

---
+++ BlackBox/_06_ClassMethod.java
@@ -0,0 +1,12 @@
+package BlackBox;
+
+public class _06_ClassMethod {
+    public static void main(String[] args) {
+        BlackBox b1 = new BlackBox();
+        b1.modelName = "까망이";
+
+        BlackBox.callServiceCenter();
+
+
+    }
+}
